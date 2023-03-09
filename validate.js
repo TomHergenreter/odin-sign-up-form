f88f951b730b0a89c4e3a40f92dc8411ce6bdef4
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-03-09 12:39:17 -0700

    Add password match validation

--- validate.js
+++ validate.js
@@ -1,9 +1,10 @@
 const showPassword = document.getElementById('show-password');
 const passwordField = document.getElementById('password');
 const passwordConfirm = document.getElementById('confirm-password');
-showPassword.addEventListener('click', (e) => {
-    
 
+console.log(passwordConfirm);
+
+showPassword.addEventListener('click', (e) => {
     if (showPassword.checked) {
         passwordField.type = 'text';
         passwordConfirm.type = 'text';
@@ -12,5 +13,21 @@ showPassword.addEventListener('click', (e) => {
         passwordConfirm.type = 'password';
     }
 });
+
+passwordConfirm.addEventListener('keyup', confirmMatch);
+passwordConfirm.addEventListener('blur', confirmMatch);
+passwordConfirm.addEventListener('focus', confirmMatch); 
+
+passwordField.addEventListener('keyup', confirmMatch);
+passwordField.addEventListener('blur', confirmMatch);
+passwordField.addEventListener('focus', confirmMatch); 
+
+function confirmMatch(){
+    if(passwordConfirm.value === passwordField.value){
+        passwordConfirm.classList.remove('invalid');
+    }else{
+        passwordConfirm.classList.add('invalid');
+    }
+};
 
 
